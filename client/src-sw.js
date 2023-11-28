a0e17f296e8baac8bb0a6a3d4e0d5d62a00713d0
Jason Savoie <flimits@gmail.com>
2023-11-28 12:33:25 -0800

    updating service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,23 +4,12 @@ import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
 import { precacheAndRoute } from 'workbox-precaching';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
-// Import the expiration plugin
-import { ExpirationPlugin } from 'workbox-expiration';
-
 precacheAndRoute(self.__WB_MANIFEST);
 
 // Register route for caching dynamic CSS and JS files.
 // The StaleWhileRevalidate strategy serves content from cache AND loads it from source if needed.
 registerRoute(
-  ({ request }) => {
-    console.log(request);
-    return (
-      // CSS
-      request.destination === 'style' ||
-      // JavaScript
-      request.destination === 'script'
-    );
-  },
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: 'static-resources',
     plugins: [
